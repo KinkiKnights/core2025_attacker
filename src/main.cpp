7f4e9ee83822249e0d5fb4b907f7e4a748feec6d
Sanjo Fumihiro <sanjofumihiro@gmail.com>
2025-05-13 06:35:21 +0900

    CoRE終了後最終ソフト０バックアップ

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "kk_driver_msg/msg/encoder.hpp"
-//#include "kk_driver_msg/msg/mouse_ctrl.hpp"
+#include "kk_driver_msg/msg/mouse_ctrl.hpp"
 #include "kk_driver_msg/msg/pwm_cmd.hpp"
 #include "kk_driver_msg/msg/motor_cmd.hpp"
 #include "kk_driver_msg/msg/bldc_cmd.hpp"
 #include "kk_driver_msg/msg/c610_cmd.hpp"
-//#include "kk_driver_msg/msg/camera_cmd.hpp"
+#include "kk_driver_msg/msg/camera_cmd.hpp"
 #include "kk_driver_msg/msg/c610_status.hpp"
 #include "kk_driver_msg/msg/core.hpp"
 #include "sensor_msgs/msg/joy.hpp"
@@ -13,20 +13,20 @@
 class core_node : public rclcpp::Node{
 public:
     rclcpp::Subscription<kk_driver_msg::msg::Core>::SharedPtr sub_joy;
-    //rclcpp::Subscription<kk_driver_msg::msg::MouseCtrl>::SharedPtr sub_mou;
     rclcpp::Subscription<kk_driver_msg::msg::Encoder>::SharedPtr sub_enc;
     rclcpp::Subscription<kk_driver_msg::msg::C610Status>::SharedPtr c610_enc;
     rclcpp::Publisher<kk_driver_msg::msg::MotorCmd>::SharedPtr motor_cmd_sub;
     rclcpp::Publisher<kk_driver_msg::msg::BldcCmd>::SharedPtr bldc_cmd_sub;
     rclcpp::Publisher<kk_driver_msg::msg::C610Cmd>::SharedPtr c610_cmd_sub;
-    //rclcpp::Publisher<kk_driver_msg::msg::CameraCmd>::SharedPtr camera_cmd_sub;
+    rclcpp::Publisher<kk_driver_msg::msg::CameraCmd>::SharedPtr camera_cmd_sub;
+    rclcpp::Subscription<kk_driver_msg::msg::MouseCtrl>::SharedPtr sub_auto;
 
     kk_driver_msg::msg::MotorCmd motor1_cmd_msg;
     kk_driver_msg::msg::MotorCmd motor2_cmd_msg;
     kk_driver_msg::msg::MotorCmd motor3_cmd_msg;
     kk_driver_msg::msg::BldcCmd bldc_cmd_msg;
     kk_driver_msg::msg::C610Cmd c610_cmd_msg;
-    //kk_driver_msg::msg::CameraCmd camera_cmd_msg;
+    
 
     core_node() : Node("core_node_node"){
 
@@ -35,11 +35,6 @@ public:
             "/core", rclcpp::QoS(10),
             std::bind(&core_node::core_callback, this, std::placeholders::_1)
         );
-        // キーボードのサブスクライバー (CoRE2用)
-        /*sub_key = this->create_subscription<kk_driver_msg::msg::MouseCtrl>(
-            "/mouses" , rclcpp::QoS(10),
-            std::bind(&core_node::mouse_callback , this, std::placeholders::_1)
-        );*/
         //エンコーダのサブスクライバー
         sub_enc = this->create_subscription<kk_driver_msg::msg::Encoder>(
             "/mtr/encoder", rclcpp::QoS(10),
@@ -52,7 +47,16 @@ public:
         motor_cmd_sub = this->create_publisher<kk_driver_msg::msg::MotorCmd>("mtr/cmd", rclcpp::QoS(10));
         bldc_cmd_sub = this->create_publisher<kk_driver_msg::msg::BldcCmd>("bldc/cmd", rclcpp::QoS(10));
         c610_cmd_sub = this->create_publisher<kk_driver_msg::msg::C610Cmd>("c610/cmd", rclcpp::QoS(10));
-        //camera_cmd_sub = this->create_publisher<kk_driver_msg::msg::CameraCmd>("camera/cmd", rclcpp::QoS(10));
+
+
+        // ★★三城★★
+        // 自動照準用のサブスクライバー 
+        sub_auto = this->create_subscription<kk_driver_msg::msg::MouseCtrl>(
+            "/auto_target" , rclcpp::QoS(10),
+            std::bind(&core_node::auto_callback , this, std::placeholders::_1)
+        );
+        camera_cmd_sub = this->create_publisher<kk_driver_msg::msg::CameraCmd>("camera", rclcpp::QoS(10));
+        // ★★三城★★
     };
     
     bool test_count = false;
@@ -60,8 +64,8 @@ public:
     int launch = 0;
     int begin = 0;
     int turn_ok = 0;
-    float x_duty;
-    float y_duty;
+    float x_duty = 0;
+    float y_duty = 0;
     float duty[4]={0.0f};
     float tt_duty = 0x0F4240;
     float front_e;
@@ -75,6 +79,7 @@ public:
     float turn_d;
     float turn_m;
     float turn_u;
+    float t_error;
     float integral;
     float derivative;
     float prev_error;
@@ -90,7 +95,7 @@ public:
     const float dt = 0.05;
     const float Kp = 0.1;
     const float Ki = 0.01;
-    const float Kd = 0.05;
+    const float Kd = 0.08;
 
     int32_t encValidate(int32_t val){
         if (val > 0x8000)
@@ -124,10 +129,10 @@ public:
 
     void Yonrin(float x, float y)       //四輪オムニのpwm値出し
     {
-        duty[0] = -y;
-        duty[1] = -x;
-        duty[2] = -x;
-        duty[3] = y;
+        duty[0] = -x;
+        duty[1] = y;
+        duty[2] = -y;
+        duty[3] = -x;
         //printf("前=%f , 右=%f ,後=%f ,左=%f\n", duty[2], duty[0], duty[1], duty[3]);
 
         // auto motor_cmd_msg = kk_driver_msg::msg::MotorCmd();
@@ -165,15 +170,15 @@ public:
     {
         if(s == 1){
             duty[0] = 130;
-            duty[1] = -130;
+            duty[1] = 130;
             duty[2] = 130;
-            duty[3] = 130;
+            duty[3] = -130;
         }
         if(s == -1){
             duty[0] = -130;
-            duty[1] = 130;
+            duty[1] = -130;
             duty[2] = -130;
-            duty[3] = -130;
+            duty[3] = 130;
         }
         
         //printf("前=%f , 右=%f ,後=%f ,左=%f\n", duty[2], duty[0], duty[1], duty[3]);
@@ -219,7 +224,7 @@ public:
             tt_duty = 0;
             turn_s = turn_c;
             printf("turn_s=%f\n", turn_s);
-            turn_s = turn_s + 1900;
+            turn_s = turn_s + 1400;
             turn_ok = 1;
         }
 
@@ -234,23 +239,31 @@ public:
         }
 
         if(turn_ok == 2){
-            //turn_d = msg->cmd[2] - 0x7F;
-            turn_m = turn_s + (msg->cmd[2] - 0x7F);
-            turn_d = turn_m - turn_c;
-            integral += turn_d * dt;
-            derivative = (turn_d - prev_error) / dt;
-            prev_error = turn_d;
-            turn_u = Kp * turn_d + Ki * integral + Kd * derivative;
-            printf("turn_u=%f\n", turn_u);
-            printf("turn_m=%f, turn_c=%f,turn_d=%f\n", turn_m, turn_c, turn_d);
-            if(turn_c >= turn_s  && turn_c <= (turn_s + 1600)){
-                tt_duty = -(turn_u * 0x1000);
-            }else if(turn_c < turn_s && turn_c >= (turn_s - 1600)){
-                tt_duty = -(turn_u * 0x1000);
-            }else{
+            t_error = msg->cmd[2];
+            turn_d = t_error - 0x7F;
+            printf("turn_d=%f\n", turn_d);
+            if(turn_c >= (turn_s - 1000) || turn_c >= (turn_s + 1000)){
+                tt_duty = turn_d * 0x2000;
+            } else{
                 tt_duty = 0x0F4240;
                 turn_ok = 0;
             }
+            //turn_m = turn_s + turn_d;
+            //turn_t = turn_m - turn_c;
+            //integral += turn_t * dt;
+            //derivative = (turn_t - prev_error) / dt;
+            //prev_error = turn_t;
+            //turn_u = Kp * turn_t + Ki * integral + Kd * derivative;
+            //printf("turn_u=%f\n", turn_u);
+            //printf("turn_m=%f, turn_c=%f,turn_t=%f\n", turn_m, turn_c, turn_t);
+            /*if(turn_c >= turn_s && turn_c <= (turn_s + 1600)){
+                tt_duty = -(turn_d * 0x3000);
+            }else if(turn_c < turn_s && turn_c >= (turn_s - 1600)){
+                tt_duty = -(turn_d * 0x3000);
+            }else{
+                tt_duty = 0x0F4240;
+                turn_ok = 0;
+            }*/
         }
 
         motor3_cmd_msg.child_id = 2;
@@ -263,20 +276,22 @@ public:
         motor3_cmd_msg.target[0] = static_cast<int32_t>(tt_duty);
 
         if(msg->cmd[3] < 0x7A || msg->cmd[3] > 0x84 || msg->cmd[4] < 0x7A || msg->cmd[4] > 0x84){ //4輪オムニ
-            x_duty = (msg->cmd[3] - 0x7F);
-            y_duty = (msg->cmd[4] - 0x7F);
+            x_duty = ((msg->cmd[3]) - 0x7F);
+            y_duty = ((msg->cmd[4]) - 0x7F);
 
             Yonrin(x_duty, y_duty);
+
+            printf("x_duty=%f, y_duty=%f\n", x_duty, y_duty);
         }else{
             x_duty = 0;
             y_duty = 0;
 
             Yonrin(x_duty, y_duty);
         }
-        if(msg->cmd[7] == 0x02){
+        if(msg->cmd[7] == 0x40){ //L2
             Senkai(1);
         }
-        if(msg->cmd[7] == 0x08){
+        if(msg->cmd[7] == 0x80){ //L1
             Senkai(-1);
         }
 
@@ -302,15 +317,15 @@ public:
             begin = 1;
         }*/
 
-        if (msg->cmd[1] == 0x04 || msg->cmd[1] == 0x05 ){ //射出モード切替
+        if (msg->cmd[1] == 0x04 || msg->cmd[1] == 0x05 || msg->cmd[1] == 0x06 || msg->cmd[1] == 0x07){ //射出モード切替
             br_left = 0xFFFF;
             br_right = 0x0000;
-        } else if(msg->cmd[7] == 0x04){
+        } else if(msg->cmd[7] == 0x20){ //R1
             br_left = 0x000000;
             br_right = 0x000000;
             begin = 0;
         } else {
-            br_left = 0x2FFF;
+            br_left = 0x3FFF;
             br_right = 0x0000;
         }
 
@@ -327,60 +342,84 @@ public:
         //printf("br_left=%f, br_right=%f\n", br_left, br_right);
 
         if (msg->cmd[1] == 0x06 || msg->cmd[1] == 0x07 ){
-            if(launch == 0){
-
-                printf("OK\n");
-                c610_cmd_msg.child_id = 0;
-                c610_cmd_msg.port.resize(1);
-                c610_cmd_msg.torque.resize(1);
-
-                // 複数ポート分の設定
-            
-                c610_cmd_msg.port[0] = 0;// ポート0のサーボ
-                c610_cmd_msg.torque[0] = 0x05DC; // ポート0の指令値
-                c610_cmd_sub->publish(c610_cmd_msg);
-
-                sleep(1.0);
-
-                c610_cmd_msg.port[0] = 0;// ポート0のサーボ
-                c610_cmd_msg.torque[0] = 0x0000; // ポート0の指令値
-                c610_cmd_sub->publish(c610_cmd_msg);
-
-                launch = 1;
-            }
-        }else{
-            launch = 0;
             c610_cmd_msg.child_id = 0;
             c610_cmd_msg.port.resize(1);
             c610_cmd_msg.torque.resize(1);
 
             // 複数ポート分の設定
+        
+            c610_cmd_msg.port[0] = 1;// ポート0のサーボ
+            c610_cmd_msg.torque[0] = 0x07FC; // ポート0の指令値
+            c610_cmd_sub->publish(c610_cmd_msg);
+
+            /*sleep(1.0);
+
             c610_cmd_msg.port[0] = 0;// ポート0のサーボ
             c610_cmd_msg.torque[0] = 0x0000; // ポート0の指令値
-        }
+            c610_cmd_sub->publish(c610_cmd_msg);
 
-        if(msg->cmd[7] == 0x80){
-            camera = camera - 1;
-            if(camera < 0){
-                camera = 3;
-            }
-        }else if(msg->cmd[7] == 0x20){
-            camera = camera + 1;
-            if(camera > 3){
-                camera = 0;
-            }
+            launch = 1;*/
+        }else{
+            c610_cmd_msg.child_id = 0;
+            c610_cmd_msg.port.resize(1);
+            c610_cmd_msg.torque.resize(1);
+
+            // 複数ポート分の設定
+            c610_cmd_msg.port[0] = 1;// ポート0のサーボ
+            c610_cmd_msg.torque[0] = 0x0000; // ポート0の指令値
+            c610_cmd_sub->publish(c610_cmd_msg);
         }
-        //camera_cmd_msg.child_id = 0;
-        //camera_cmd_msg.port[0] = camera;
        
         motor_cmd_sub->publish(motor1_cmd_msg);
         motor_cmd_sub->publish(motor2_cmd_msg);
         motor_cmd_sub->publish(motor3_cmd_msg);
         bldc_cmd_sub->publish(bldc_cmd_msg);
         c610_cmd_sub->publish(c610_cmd_msg);
-        //camera_cmd_sub->publish(camera_cmd_msg);
+        
+        // ★★三城★★
+        kk_driver_msg::msg::CameraCmd camera_cmd_msg;
+        if(msg->cmd[7] == 0x02){
+            // ボタンを押している間だけ2カメ表示
+            camera_cmd_msg.camera_id = 1;
+        } else{
+            
+            camera_cmd_msg.camera_id = 0;
+        }
+        camera_cmd_sub->publish(camera_cmd_msg);
 
     }
+
+    // ★★三城★★
+    void auto_callback(const kk_driver_msg::msg::MouseCtrl::SharedPtr msg){
+        uint8_t ctrl = msg->x;
+        printf("要求制御量：%d\n", ctrl);
+
+        if(turn_ok == 3){
+            turn_d = ctrl - 0x7F;
+            if(turn_c >= turn_s && turn_c <= (turn_s + 1600)){
+                tt_duty = -(turn_d * 0x3000);
+            }else if(turn_c < turn_s && turn_c >= (turn_s - 1600)){
+                tt_duty = -(turn_d * 0x3000);
+            }else{
+                tt_duty = 0x0F4240;
+                turn_ok = 0;
+            }
+
+            motor3_cmd_msg.child_id = 2;
+            motor3_cmd_msg.port.resize(1);
+            motor3_cmd_msg.ctrl.resize(1);
+            motor3_cmd_msg.target.resize(1);
+
+            motor3_cmd_msg.port[0] = 0;
+            motor3_cmd_msg.ctrl[0] = 1;
+            motor3_cmd_msg.target[0] = static_cast<int32_t>(tt_duty);
+
+            motor_cmd_sub->publish(motor3_cmd_msg);
+        }
+        // あとはいい感じにお願いします。
+        // 特定のボタン押している間だけ取得した値に従って自動で首フルとかでもいいかも
+    }
+    // ★★三城★★
 
     /* マウス入力の処理
     void mouse_callback(const kk_driver_msg::msg::MouseCtrl::SharedPtr msg){
